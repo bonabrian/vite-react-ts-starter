cfc30bba75ed5e502e932b096f849d674904b825
bonabrian <bonabriansiagian@gmail.com>
2022-06-07 06:55:19 +0700

    chore: add .eslintrc.js file

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 13,
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['react', '@typescript-eslint', 'unused-imports'],
+  ignorePatterns: ['.eslintrc.*', 'vite.config.*'],
+  rules: {
+    semi: 'off',
+    '@typescript-eslint/semi': ['error', 'never'],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
+    'react/jsx-one-expression-per-line': 0,
+  },
+}
